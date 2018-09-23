eaa6745778150597b4dbacce121af5f3bd67f2b3
yurochka <yurychernyak9@gmail.com>
2018-09-23 20:14:12 +0300

    second

--- routes/reservations.js
+++ routes/reservations.js
@@ -16,8 +16,8 @@ router.post('/', (req,res)=>{
     }
     knex('reservations').insert({
       table_id: req.body.table_id,
-      start_time: 'sept 24',
-      end_time: 'sept25',
+      start_time: req.body.start_time,
+      end_time: req.body.end_time,
       guests: req.body.guests
     })
     .then(()=>{
@@ -36,12 +36,33 @@ router.get('/new/:id', (req,res)=>{
   res.render('new', {data: req.params.id})
 })
 
+router.get('/update/:id', (req,res)=>{
+  knex('reservations').where('id', req.params.id).then((data)=>{
+    //console.log(data);
+      res.render('edit', {data: data[0]});
+  })
+
+})
+
 
 router.delete('/delete/:id', (req,res)=>{
   console.log('delete'+req.params.id)
   knex('reservations').delete().where('id', req.params.id)
   .then((data)=>{
     res.send('deleted'+data+' reservation');
+  })
+
+})
+
+router.post('/update',(req,res)=>{//////////updating
+  console.log(req.body)
+  knex('reservations').update({
+    guests: req.body.guests,
+    start_time: req.body.start_time,
+    end_time: req.body.end_time
+  }).where('id', req.body.current_id).then(()=>{
+    res.redirect('/reservations');
+
   })
 
 })

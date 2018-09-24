7ac4258248c9d06a25a9154db8db1a2a7204a181
yurochka <yurychernyak9@gmail.com>
2018-09-24 15:48:41 +0300

    testing

---
+++ test/appTesting.js
@@ -0,0 +1,19 @@
+const assert = require('chai').assert;
+const kx = require('../knexfile.js').development.connection
+
+
+describe('App', ()=>{
+  it('Host checking', ()=>{
+    assert.equal(kx.host, 'localhost')
+  })
+  it('user checking', ()=>{
+    assert.typeOf(kx.user, 'string')
+  })
+  it('password checking', ()=>{
+    assert.isAbove(kx.password, 111)
+  })
+  it('database checking', ()=>{
+    assert.strictEqual(kx.database, 'restaurant')
+  })
+
+})
